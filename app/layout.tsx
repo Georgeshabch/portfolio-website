0c5ef467af0f3d36ef8779ed43c3ed512b6f76c0
Georges Habchi <georges.habchi2000@gmail.com>
2025-04-14 17:17:20 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,13 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
-import ActiveSectionContextProvider from "@/context/active-section-contex";
+import ActiveSectionContextProvider from "@/context/active-section-contex"; 
 import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
 
-
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata: Metadata = {
   title: "George Habchi",
-  description: "George is a full-stack developer with 1 years of experience",
+  description: "George is a full-stack developer with 1 year of experience",
 };
 
 export default function RootLayout({
@@ -30,16 +17,18 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="!scroll-smooth">
-      <body data-new-gr-c-s-check-loaded="14.1231.0"
-        data-gr-ext-installed="" className="relative bg-gray-50 pt-28 text-gray-950 transition-all 
-                         sm:pt-36 ">
-        <div className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem]
-                     rounded-full bg-[#946263] blur-[10rem] dark:bg-[#fbe2e3] sm:w-[68.75rem]"></div>
-        <div className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] rounded-full bg-[#676394]
-                        blur-[10rem] dark:bg-[#dbd7fb] sm:left-[-30rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem]
-                        xl:left-[-15rem] 2xl:left-[-5rem]"></div>
-
-
+      <body
+        className="relative bg-gray-50 pt-28 text-gray-950 transition-all sm:pt-36"
+      >
+        <div
+          className="absolute right-[11rem] top-[-6rem] -z-10 h-[31.25rem] w-[31.25rem] 
+          rounded-full bg-[#946263] blur-[10rem] dark:bg-[#fbe2e3] sm:w-[68.75rem]"
+        />
+        <div
+          className="absolute left-[-35rem] top-[-1rem] -z-10 h-[31.25rem] w-[50rem] 
+          rounded-full bg-[#676394] blur-[10rem] dark:bg-[#dbd7fb] sm:left-[-30rem] 
+          sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem]"
+        />
         <ActiveSectionContextProvider>
           <Header />
           {children}
